462bab204e57091b7811028d47cc3bea0675dcc9
jonrhuang <jonrhuang@gmail.com>
2025-12-31 00:06:00 -0500

    sightings page showing data

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 import http from 'node:http'
 import { serveStatic } from './utils/serveStatic.js'
+import { handleGet } from './handlers/routeHandlers.js'
 
 const PORT = 8000
 
 const __dirname = import.meta.dirname
 
 const server = http.createServer(async (req, res) => {
+    if (req.url === '/api') {
+        if (req.method === 'GET') {
+            return await handleGet(res)
+        }
+    }
+
     await serveStatic(req, res, __dirname)
 }) 
 
